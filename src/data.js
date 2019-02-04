679dca7d5407c7bb536a5060dfca5befe72b39df
chinedu onyeoka <chinedu.onyeoka@gmail.com>
2019-02-04 22:54:02 +0100

    added state to productlist

--- src/data.js
+++ src/data.js
@@ -4,9 +4,116 @@ export const storeProducts = [
         title: "Google Pixel - Black",
         img: "img/product-1.png",
         price: 10,
+        company: "bondexpress",
+        info:
+        "we are  working on the best product to fulfill any mabndate neede for our product,\
+         colllaboration with some  the smartesdt people libvinng within  wdxhcbcjs ",
+         inCart: false,
+         count: 0,
+         total: 0
+    },
+    {
+        id:2,
+        title: "Google Pixel - Black",
+        img: "img/product-2.png",
+        price: 10,
+        company: "google",
+        info:
+        "we are  working on the best product to fulfill any mabndate neede for our product,\
+         colllaboration with some  the smartesdt people libvinng within  wdxhcbcjs ",
+         inCart: false,
+         count: 0,
+         total: 0
+    },
+    {
+        id:3,
+        title: "Google Pixel - Black",
+        img: "img/product-3.png",
+        price: 18,
+        company: "samsung",
+        info:
+        "we are  working on the best product to fulfill any mabndate neede for our product,\
+         colllaboration with some  the smartesdt people libvinng within  wdxhcbcjs ",
+         inCart: false,
+         count: 0,
+         total: 0
+    },
+    {
+        id:4,
+        title: "Google mansad - Black",
+        img: "img/product-4.png",
+        price: 10,
+        company: "parewne",
+        info:
+        "we are  working on the best product to fulfill any mabndate neede for our product,\
+         colllaboration with some  the smartesdt people libvinng within  wdxhcbcjs ",
+         inCart: false,
+         count: 0,
+         total: 0
+    },
+    {
+        id:6,
+        title: "Apple X5",
+        img: "img/product-1.png",
+        price: 450,
+        company: "bondexpress",
+        info:
+        "we are  working on the best product to fulfill any mabndate neede for our product,\
+         colllaboration with some  the smartesdt people libvinng within  wdxhcbcjs ",
+         inCart: false,
+         count: 0,
+         total: 0
+    },
+    {
+        id:6,
+        title: "Google Pixel - Black",
+        img: "img/product-1.png",
+        price: 10,
         company: bondexpress,
         info:
         "we are  working on the best product to fulfill any mabndate neede for our product,\
-         colllaboration with some  the smartesdt people libvinng within  wdxhcbcjs "
-    }
+         colllaboration with some  the smartesdt people libvinng within  wdxhcbcjs ",
+         inCart: false,
+         count: 0,
+         total: 0
+    },
+    {
+        id:7,
+        title: "Google samsiun - Black",
+        img: "img/product-1.png",
+        price: 230,
+        company: "bondexpress",
+        info:
+        "we are  working on the best product to fulfill any mabndate neede for our product,\
+         colllaboration with some  the smartesdt people libvinng within  wdxhcbcjs ",
+         inCart: false,
+         count: 0,
+         total: 0
+    },
+    {
+        id:8,
+        title: "infinix Pixel - Black",
+        img: "img/product-1.png",
+        price: 10,
+        company: "bondexpress",
+        info:
+        "we are  working on the best product to fulfill any mabndate neede for our product,\
+         colllaboration with some  the smartesdt people libvinng within  wdxhcbcjs ",
+         inCart: false,
+         count: 0,
+         total: 0
+    },
+    {
+        id:33,
+        title: "Google Pixel - Black",
+        img: "img/product-1.png",
+        price: 10,
+        company: "bondexpress",
+        info:
+        "we are  working on the best product to fulfill any mabndate neede for our product,\
+         colllaboration with some  the smartesdt people libvinng within  wdxhcbcjs ",
+         inCart: false,
+         count: 0,
+         total: 0
+    },
 ]
