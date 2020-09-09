f829cb0afad979c644c9f3db5b360d81ca72a75d
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-09-09 18:59:52 +0100

    wip

--- src/data.js
+++ src/data.js
@@ -1,211 +1,212 @@
 export const storeProducts = [
     {
-        id:1,
+        id: 1,
         title: "Google Pixel - Black",
         img: "img/product-1.png",
         price: 10,
         company: "bondexpress",
         info:
-        "we are  working on the best product to fulfill any mabndate neede for our product,\
-         colllaboration with some  the smartesdt people libvinng within  wdxhcbcjs ",
-         inCart: false,
-         count: 0,
-         total: 0
+            "LYNACLOTHINGS\
+         For more information please call 08099699990",
+        inCart: false,
+        count: 0,
+        total: 0,
     },
     {
-        id:2,
+        id: 2,
         title: "Google Pixel - Black",
         img: "img/product-2.png",
         price: 10,
         company: "google",
         info:
-        "we are  working on the best product to fulfill any mabndate neede for our product,\
-         colllaboration with some  the smartesdt people libvinng within  wdxhcbcjs ",
-         inCart: false,
-         count: 0,
-         total: 0
+            "LYNACLOTHINGS,\
+        For more information please call 08099699990",
+        inCart: false,
+        count: 0,
+        total: 0,
     },
     {
-        id:3,
+        id: 3,
         title: "Google Pixel - Black",
         img: "img/product-3.png",
         price: 18,
-        company: "samsung",
+        company: "LYNA WEARS",
         info:
-        "we are  working on the best product to fulfill any mabndate neede for our product,\
-         colllaboration with some  the smartesdt people libvinng within  wdxhcbcjs ",
-         inCart: false,
-         count: 0,
-         total: 0
+            "LYNACLOTHINGS,\
+         For more information please call 08099699990",
+        inCart: false,
+        count: 0,
+        total: 0,
     },
     {
-        id:4,
+        id: 4,
         title: "Google mansad - Black",
         img: "img/product-4.png",
         price: 10,
         company: "parewne",
         info:
-        "we are  working on the best product to fulfill any mabndate neede for our product,\
-         colllaboration with some  the smartesdt people libvinng within  wdxhcbcjs ",
-         inCart: false,
-         count: 0,
-         total: 0
+            "LYNACLOTHINGS,\
+         For more information please call 08099699990",
+        inCart: false,
+        count: 0,
+        total: 0,
     },
     {
-        id:22,
+        id: 22,
         title: "Apple X5",
         img: "img/product-5.png",
         price: 450,
         company: "bondexpress",
         info:
-        "we are  working on the best product to fulfill any mabndate neede for our product,\
-         colllaboration with some  the smartesdt people libvinng within  wdxhcbcjs ",
-         inCart: false,
-         count: 0,
-         total: 0
+            "LYNACLOTHINGS,\
+        For more information please call 08099699990",
+        inCart: false,
+        count: 0,
+        total: 0,
     },
     {
-        id:6,
+        id: 6,
         title: "Google Pixel - Black",
         img: "img/product-6.png",
         price: 10,
         company: "bondexpress",
         info:
-        "we are  working on the best product to fulfill any mabndate neede for our product,\
-         colllaboration with some  the smartesdt people libvinng within  wdxhcbcjs ",
-         inCart: false,
-         count: 0,
-         total: 0
+            "LYNACLOTHINGS,\
+         For more information please call 08099699990",
+        inCart: false,
+        count: 0,
+        total: 0,
     },
     {
-        id:7,
+        id: 7,
         title: "Google samsiun - Black",
         img: "img/product-7.png",
         price: 230,
         company: "bondexpress",
         info:
-        "we are  working on the best product to fulfill any mabndate neede for our product,\
-         colllaboration with some  the smartesdt people libvinng within  wdxhcbcjs ",
-         inCart: false,
-         count: 0,
-         total: 0
+            "LYNACLOTHINGS,\
+        For more information please call 08099699990",
+        inCart: false,
+        count: 0,
+        total: 0,
     },
     {
-        id:8,
+        id: 8,
         title: "infinix Pixel - Black",
         img: "img/product-8.png",
         price: 10,
         company: "bondexpress",
         info:
-        "we are  working on the best product to fulfill any mabndate neede for our product,\
-         colllaboration with some  the smartesdt people libvinng within  wdxhcbcjs ",
-         inCart: false,
-         count: 0,
-         total: 0
+            "LYNACLOTHINGS,\
+         For more information please call 08099699990",
+        inCart: false,
+        count: 0,
+        total: 0,
     },
     {
-        id:33,
+        id: 33,
         title: "Google Pixel - Black",
         img: "img/product-9.png",
         price: 10,
         company: "bondexpress",
         info:
-        "we are  working on the best product to fulfill any mabndate neede for our product,\
-         colllaboration with some  the smartesdt people libvinng within  wdxhcbcjs ",
-         inCart: false,
-         count: 0,
-         total: 0
+            "LYNACLOTHINGS,\
+         For more information please call 08099699990",
+        inCart: false,
+        count: 0,
+        total: 0,
     },
     {
-        id:31,
+        id: 31,
         title: "Google Pixel - Black",
         img: "img/product-10.png",
         price: 10,
         company: "bondexpress",
         info:
-        "we are  working on the best product to fulfill any mabndate neede for our product,\
-         colllaboration with some  the smartesdt people libvinng within  wdxhcbcjs ",
-         inCart: false,
-         count: 0,
-         total: 0
+            "LYNACLOTHINGS,\
+         For more information please call 08099699990",
+        inCart: false,
+        count: 0,
+        total: 0,
     },
 
     {
-        id:32,
+        id: 32,
         title: "Google Pixel - Black",
         img: "img/product-11.png",
         price: 10,
         company: "bondexpress",
         info:
-        "we are  working on the best product to fulfill any mabndate neede for our product,\
-         colllaboration with some  the smartesdt people libvinng within  wdxhcbcjs ",
-         inCart: false,
-         count: 0,
-         total: 0
+            "LYNACLOTHINGS,\
+        For more information please call 08099699990",
+
+        inCart: false,
+        count: 0,
+        total: 0,
     },
     {
-        id:34,
+        id: 34,
         title: "Google Pixel - Black",
         img: "img/product-12.png",
         price: 10,
         company: "bondexpress",
         info:
-        "we are  working on the best product to fulfill any mabndate neede for our product,\
-         colllaboration with some  the smartesdt people libvinng within  wdxhcbcjs ",
-         inCart: false,
-         count: 0,
-         total: 0
+            "LYNACLOTHINGS,\
+        For more information please call 08099699990",
+        inCart: false,
+        count: 0,
+        total: 0,
     },
     {
-        id:35,
+        id: 35,
         title: "Google Pixel - Black",
         img: "img/product-13.png",
         price: 10,
         company: "bondexpress",
         info:
-        "we are  working on the best product to fulfill any mabndate neede for our product,\
-         colllaboration with some  the smartesdt people libvinng within  wdxhcbcjs ",
-         inCart: false,
-         count: 0,
-         total: 0
+            "LYNACLOTHINGS,\
+         For more information please call 08099699990",
+        inCart: false,
+        count: 0,
+        total: 0,
     },
     {
-        id:36,
+        id: 36,
         title: "Google Pixel - Black",
         img: "img/product-14.png",
         price: 10,
         company: "bondexpress",
         info:
-        "we are  working on the best product to fulfill any mabndate neede for our product,\
-         colllaboration with some  the smartesdt people libvinng within  wdxhcbcjs ",
-         inCart: false,
-         count: 0,
-         total: 0
+            "LYNACLOTHINGS,\
+         For more information please call 08099699990",
+        inCart: false,
+        count: 0,
+        total: 0,
     },
     {
-        id:37,
+        id: 37,
         title: "Google Pixel - Black",
         img: "img/product-15.png",
         price: 10,
         company: "bondexpress",
         info:
-        "we are  working on the best product to fulfill any mabndate neede for our product,\
-         colllaboration with some  the smartesdt people libvinng within  wdxhcbcjs ",
-         inCart: false,
-         count: 0,
-         total: 0
+            "LYNACLOTHINGS,\
+         For more information please call 08099699990",
+        inCart: false,
+        count: 0,
+        total: 0,
     },
 ];
 
 export const detailProduct = {
-    id:1,
+    id: 1,
     title: "Google-pixel black",
     img: "img/product-1.png",
-    price:10,
-    company:"google",
-    info:
-     "understanding the gundamentakls the fundation of ensuring we sell a poweerful product better than that we are better than that",
-     inCart:false,
-     count:0,
-     total:0
+    price: 10,
+    company: "google",
+    info: "LYNACLOTHINGS,\
+    For more information please call 08099699990",
+    inCart: false,
+    count: 0,
+    total: 0,
 };
